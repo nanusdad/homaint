4f89dba54cb1a3c8ffbbc76b425ccf4d8c0cda42
Arun Zechariah <arun@zechariah.net>
2010-12-13 23:52:42 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homaint::Application.config.secret_token = '9181988bd1a0dc03f7f5241f5e627144a892592642a371377922c82bc99d12e13db5bf118a313244fa97cc5b8218c4507fc7a19c8ed922898ab8251dc1d3d25d'
